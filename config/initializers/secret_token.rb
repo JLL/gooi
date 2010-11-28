a0412108acd32b8c12138af0ccbec6b74cacdc01
Aurelien DROUET <aureliendrouet@z238094.dynamic.ppp.asahi-net.or.jp>
2010-11-28 23:17:50 +0900

    New app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goi::Application.config.secret_token = '856bf7e19b7da788c92a49d3387b2f2df0ac935ab0f46fe648096d382ad7b7130c01ca7c45c44b27a601138c7b8d95e6ace586b4a48d37d130e84757b97f06fa'
